4848e5784a24a61717dcbb8db88a39a8472ebc63
Rafael Sawiris <rafaelsawiris@Rafaels-MacBook-Pro.local>
2024-01-03 16:59:38 -0800

    finalized unique id and css

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -2,8 +2,14 @@ import { useState } from 'react'
 import { puppyList } from './data.js'
 import './App.css'
 
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+  
 
   console.log(puppies)
 
@@ -12,12 +18,22 @@ function App() {
     <>
       <div className = 'App'>
         {
+          
           puppies.map((puppy) => {
             return (
-              <p key={puppy.id}>{puppy.name}</p>
+              <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
             )
           })
         }
+        {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )} 
       </div>
     
     </>
