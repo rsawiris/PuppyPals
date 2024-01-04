892e33d3cbd4d6239038a4e865e761fd1cb81b3f
Rafael Sawiris <rafaelsawiris@Rafaels-MacBook-Pro.local>
2024-01-03 16:29:41 -0800

    added puppies list

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -1,13 +1,23 @@
-
+import { useState } from 'react'
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+
+  console.log(puppies)
 
 
   return (
     <>
-      <div>
-        
+      <div className = 'App'>
+        {
+          puppies.map((puppy) => {
+            return (
+              <p key={puppy.id}>{puppy.name}</p>
+            )
+          })
+        }
       </div>
     
     </>
